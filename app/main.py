23d31585a70211aab3dda66c6105f7e5fb547569
Elijah Kimani <elijahkimani1293@gmail.com>
2025-03-27 15:53:20 +0300

    first commit

--- app/main.py
+++ app/main.py
@@ -5,14 +5,11 @@ from app.routers import auth, cargo, tracking, notifications,users
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",  # Local development
-    "https://cargo-frontend-dusky.vercel.app"  # Deployed frontend
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,  # Allow cookies/auth headers
     allow_methods=["*"],  # Allow all request methods
     allow_headers=["*"],  # Allow all headers
